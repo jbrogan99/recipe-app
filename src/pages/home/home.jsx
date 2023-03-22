3e02659d097b34dd6af971305c98f854f3b137c0
jbrogan99 <jack.brogan99@gmail.com>
2023-03-22 16:50:13 +0000

    main section updated for home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,6 +5,12 @@ import banner from "../../images/green.jpg";
 import { RecipeFinder } from "../../recipie-finder";
 import beanChilliWrap from "../../images/bean_and_rice_burrito_33565_16x9.jpg";
 import pork from "../../images/chilli-pork.jpg";
+import soup from "../../images/soup-min.jpg";
+import salad from "../../images/salad.jpg";
+import pasta1 from "../../images/healthy-pasta-recipes.jpeg";
+import pasta2 from "../../images/healthy-pasta.jpg";
+import burger from "../../images/hamburger-food.jpg";
+import carbonara from "../../images/carbonara.jpg";
 export const Home = () => {
   const handleDragStart = (e) => e.preventDefault();
   const items = [
@@ -49,13 +55,43 @@ export const Home = () => {
       <main className="pastel-background recipe-finder-container">
         <RecipeFinder
           title="Feeling"
-          lastLetter={` ${"Hungover"}`}
+          lastLetter={` ${"Hungover?"}`}
           slogan="Have you had one too many?"
           url1={beanChilliWrap}
           url2={pork}
           info="Enjoy some of our easy, tasty and nutritional dishes, cooks in under 10 minutes"
           alt1="bean and rice wrap"
           alt2="chilli pork"
+        />
+        <RecipeFinder
+          title="Hows the"
+          lastLetter={` ${"Weather?"}`}
+          slogan="Is it wet and rainy, or is the sun beaming down?"
+          url1={soup}
+          url2={salad}
+          info="Whatever the weather, we’ve got the dishes you need!"
+          alt1="tomato soup"
+          alt2="chicken salad"
+        />
+        <RecipeFinder
+          title="Food for"
+          lastLetter={` ${"Work?"}`}
+          slogan="Sick of spending money on overpriced foods at work? "
+          url1={pasta1}
+          url2={pasta2}
+          info="We have a great selection of healthy re-heatable meals for you to take into work! "
+          alt1="beef pasta with sweetcorn"
+          alt2="tagliatelle pasta with herbs"
+        />
+        <RecipeFinder
+          title="No Time to "
+          lastLetter={` ${"Travel?"}`}
+          slogan="Do you have to travel to find the right ingredients? "
+          url1={burger}
+          url2={carbonara}
+          info="All the ingredients for the dishes can be found in any budget supermarket"
+          alt1="hamburger"
+          alt2="carbonara"
         />
       </main>
     </>
