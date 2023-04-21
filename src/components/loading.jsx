926df04733ea030e92be19ef7bd2c49e9fe9d723
jbrogan99 <jack.brogan99@gmail.com>
2023-04-21 18:30:21 +0100

    travel fix and spinner updated

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -4,7 +4,6 @@ import { ReactComponent as Spinner } from "../images/Spinner-new.svg";
 const Loading = () => {
   return (
     <div className="loading">
-      <p>Hold on, where just getting the recipes for you now, wont be long!</p>
       <Spinner className="spinner" />
     </div>
   );
